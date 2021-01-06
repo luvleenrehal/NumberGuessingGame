3ee2c7b60f7b9c191e6348b00716e16ef75e30aa
luvleenrehal <73304147+luvleenrehal@users.noreply.github.com>
2021-01-06 14:16:23 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GuessingNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int numToWin = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess number between 0 - 100");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > numToWin)
+                {
+                    Console.WriteLine("Too High: Guess a lower number");
+                }
+                else if (i < numToWin)
+                {
+                    Console.WriteLine("Too Low: Guess a higher number");
+                }
+                else if (i == numToWin)
+                {
+                    Console.WriteLine("You Win!");
+                    win = true;
+
+                }
+                Console.WriteLine();
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing");
+            Console.Write("Press any key to finish.");
+            Console.ReadKey(true);
+        }
+    }
+}
